c7f91d491113857cae65f3aa150fcbca8f9433e7
Daniel Li <dan@danyll.com>
2017-02-01 21:57:45 +0000

    Updated package.json with version constraint

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'brewhk:follower-material-ui',
-  version: '0.0.2',
+  version: '0.0.4',
   summary: 'Material UI components for the brewhk:follower package',
   git: 'https://github.com/brewhk/follower-material-ui.git',
   documentation: 'README.md'
@@ -13,6 +13,6 @@ Npm.depends({
 Package.onUse(function(api) {
   api.versionsFrom('1.4.2.3');
   api.use('ecmascript');
-  api.use('brewhk:follower');
+  api.use('brewhk:follower@1.0.0');
   api.mainModule('client/main.js', 'client');
 });
